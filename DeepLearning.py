0bf429576e84fae8181830d2975d700441af8382
Redcoder815 <artificial815@gmail.com>
2026-01-09 17:18:42 +0100

    tensor flow

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,17 +1,30 @@
 import torch
-import torch.nn as nn
+from torch import nn
+class LinearRegressionModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.weight = nn.Parameter(data=torch.randn(1, 
+                                              requires_grad=True,
+                                              dtype=torch.float
+                                              ))
+    
+    self.bias = nn.Parameter(data=torch.randn(1, 
+                                              requires_grad=True,
+                                              dtype=torch.float
+                                              ))
 
-class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        # Learnable weight and bias
-        self.weight = nn.Parameter(torch.randn(1, dtype=torch.float, requires_grad=True))
-        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float, requires_grad=True))
+  def forward(self, x):
+    return self.weight * x + self.bias
 
-    def forward(self, x):
-        return x * self.weight + self.bias
-
-# Example usage
-model = MyModel()
-print("Weight:", model.weight.item())
-print("Bias:", model.bias.item())
+# ## Option 2
+# class LinearRegressionModel(nn.Module):
+#   def __init__(self):
+#     super().__init__()
+#     self.linear_layer = nn.Linear(in_features = 1,
+#                                   out_features = 1)
+#   def forward(self,x : torch.Tensor) -> torch.Tensor:
+#     return self.linear_layer(x)
+  
+torch.manual_seed(42)
+model_1 = LinearRegressionModel()
+model_1,model_1.state_dict()
