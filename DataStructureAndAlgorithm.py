8687728c5b251705b0f069da34e7af150e2529cc
Redcoder815 <artificial815@gmail.com>
2026-02-17 12:22:51 +0100

    tensor

--- DataStructureAndAlgorithm.py
+++ DataStructureAndAlgorithm.py
@@ -1,11 +1,22 @@
-def sum(n):
-    
-    # base condition
-    if n == 1:
-        return 1
-    
-    return n + sum(n - 1)
+# A Python 3 program to
+# demonstrate working of
+# recursion
 
-if __name__ == "__main__":
-    n = 5
-    print(sum(n))
+
+def printFun(test):
+
+    if (test < 1):
+        return
+    else:
+
+        print(test, end=" ")
+        printFun(test-1)  # statement 2
+        print(test, end=" ")
+        return
+
+# Driver Code
+test = 3
+printFun(test)
+
+# This code is contributed by
+# Smitha Dinesh Semwal
